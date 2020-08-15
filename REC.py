1516082776615ddc1001938282f5462592d206d2
WildmanJames <dman13195@gmail.com>
2020-08-15 16:50:14 -0400

    working on the data inputs!

--- REC.py
+++ REC.py
@@ -3,10 +3,19 @@
 # It must be compatible with raspbian and windows
 # I would like to rely on as few modules as possible
 
+import board
+import time
+import busio
+import adafruit_bmp3xx
+import adafruit_icm20x
 import pandas as pd
 import pathlib as pl
 import datetime as dt
 import logging
+
+i2c = busio.I2C(board.SCL, board.SDA)
+bmp388 = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+icm20649 = adafruit_icm20x.ICM20649(i2c)
 
 # Gotta learn logging
 
@@ -35,59 +44,60 @@ def determine_landed():
 
 def get_bta():
     '''
-    This function will interact with the "--board--" and return the barometric pressure and temperature detected by the
+    This function will interact with bmp388 and return the barometric pressure and temperature detected by the
     sensor. It will also return the current altitude calculated by its built in altitude function
     '''
-    # b =  speak to "--device--" to get current data point
-    # t =  speak to "--device--" to get current data point
-    # a =  speak to "--device--" to get current data point
 
-    # test
+    # b =  speak to bmp388 to get current data point
+    # t =  speak to bmp388 to get current data point
+    # a =  speak to bmp388 to get current data point
+
     b = pd.Series([], dtype = 'float64')
     t = pd.Series([], dtype = 'float64')
     a = pd.Series([], dtype = 'float64')
+
+    # test
+
     for _ in range(10):
         b[_] = 2 * _
         t[_] = 2 * _
         a[_] = 2 * _
     return b, t, a
 
-def get_accel():
+    # end test
+
+def get_accel_gyro():
     '''
-        This function will interact with the "--board--" and return the accelerometer data detected by the
+        This function will interact with icm20649 and return the accelerometer and gyro data detected by the
         sensor
     '''
-    # x =  speak to "--device--" to get current data point
-    # y =  speak to "--device--" to get current data point
-    # z =  speak to "--device--" to get current data point
+
+    a_x = pd.Series([], dtype = 'float64')
+    a_y = pd.Series([], dtype = 'float64')
+    a_z = pd.Series([], dtype = 'float64')
+    g_x = pd.Series([], dtype = 'float64')
+    g_y = pd.Series([], dtype = 'float64')
+    g_z = pd.Series([], dtype = 'float64')
+
+    # a_x =  speak to icm20649 to get current data point
+    # a_y =  speak to icm20649 to get current data point
+    # a_z =  speak to icm20649 to get current data point
+    # g_x =  speak to icm20649 to get current data point
+    # g_y =  speak to icm20649 to get current data point
+    # g_z =  speak to icm20649 to get current data point
 
     # test
-    x = pd.Series([], dtype = 'float64')
-    y = pd.Series([], dtype = 'float64')
-    z = pd.Series([], dtype = 'float64')
-    for _ in range(10):
-        x[_] = 2 * _
-        y[_] = 2 * _
-        z[_] = 2 * _
-    return x, y, z
 
-def get_gyro():
-    '''
-        This function will interact with the "--board--" and return the gyro data detected by the sensor
-    '''
-    # x =  speak to "--device--" to get current data point
-    # y =  speak to "--device--" to get current data point
-    # z =  speak to "--device--" to get current data point
-
-    # test
-    x = pd.Series([], dtype = 'float64')
-    y = pd.Series([], dtype = 'float64')
-    z = pd.Series([], dtype = 'float64')
     for _ in range(10):
-        x[_] = 2 * _
-        y[_] = 2 * _
-        z[_] = 2 * _
-    return x, y, z
+        a_x[_] = 2 * _
+        a_y[_] = 2 * _
+        a_z[_] = 2 * _
+        g_x[_] = 2 * _
+        g_y[_] = 2 * _
+        g_z[_] = 2 * _
+
+    # end test
+    return a_x, a_y, a_z, g_x, g_y, g_z
 
 def mk_run_dir():
     '''
@@ -118,8 +128,7 @@ def gather_store_data():
     run_path = mk_run_dir()
 
     b, t, a = get_bta()
-    a_x, a_y, a_z = get_accel()
-    g_x, g_y, g_z = get_gyro()
+    a_x, a_y, a_z, g_x, g_y, g_z = get_accel_gyro()
 
     data_dict = {
             "Barometric Pressure":b,
