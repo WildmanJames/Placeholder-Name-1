b6022b9db79d53e48ad84761724c5f06bb65ad6b
WildmanJames <dman13195@gmail.com>
2020-08-22 16:18:34 -0400

    Another way

--- REC.py
+++ REC.py
@@ -27,6 +27,8 @@ icm20649 = adafruit_icm20x.ICM20649(i2c)
 
 def main():
 
+    print('Program start!')
+
     setup_boards()
 
     gather_store_data()
@@ -35,6 +37,8 @@ def main():
     # figure out how to keep only data from 3 seconds before launch
     # perhaps log data from when program starts and determine time of launch, then sort the big csv and remove data
     # from before the launch
+
+    print('Program end!')
 
     return None
 
@@ -50,9 +54,10 @@ def setup_boards():
 
     icm20649.reset()
     icm20649.initialize()
+    print('Initializing, please wait...')
     time.sleep(5)
 
-
+    print('Begin test!')
 
 def determine_launch():
     """
@@ -66,47 +71,18 @@ def determine_landed():
     """
     return None
 
-def get_bta():
+def get_data():
     """
     This function will interact with bmp388 and return the barometric pressure and temperature detected by the
     sensor. It will also return the current altitude calculated by its built in altitude function
+
+    This function will interact with icm20649 and return the accelerometer and gyro data detected by the
+    sensor
     """
 
     # b =  speak to bmp388 to get current data point
     # t =  speak to bmp388 to get current data point
     # a =  speak to bmp388 to get current data point
-
-    b = pd.Series([], dtype = 'float64')
-    t = pd.Series([], dtype = 'float64')
-    a = pd.Series([], dtype = 'float64')
-
-    # test
-
-    _ = 0
-    while True:
-        b[_] = bmp388.pressure
-        t[_] = bmp388.temperature
-        a[_] = bmp388.altitude
-        _ += 1
-        time.sleep(0.1)
-        if _ is 100: break
-    return b, t, a
-
-    # end test
-
-def get_accel_gyro():
-    """
-        This function will interact with icm20649 and return the accelerometer and gyro data detected by the
-        sensor
-    """
-
-    a_x = pd.Series([], dtype = 'float64')
-    a_y = pd.Series([], dtype = 'float64')
-    a_z = pd.Series([], dtype = 'float64')
-    g_x = pd.Series([], dtype = 'float64')
-    g_y = pd.Series([], dtype = 'float64')
-    g_z = pd.Series([], dtype = 'float64')
-
     # a_x =  speak to icm20649 to get current data point
     # a_y =  speak to icm20649 to get current data point
     # a_z =  speak to icm20649 to get current data point
@@ -114,20 +90,42 @@ def get_accel_gyro():
     # g_y =  speak to icm20649 to get current data point
     # g_z =  speak to icm20649 to get current data point
 
+    b = pd.Series([], dtype = 'float16')
+    t = pd.Series([], dtype = 'float16')
+    a = pd.Series([], dtype = 'float16')
+    a_x = pd.Series([], dtype = 'float16')
+    a_y = pd.Series([], dtype = 'float16')
+    a_z = pd.Series([], dtype = 'float16')
+    g_x = pd.Series([], dtype = 'float16')
+    g_y = pd.Series([], dtype = 'float16')
+    g_z = pd.Series([], dtype = 'float16')
+
     # test
+    start = time.time()
     _ = 0
     while True:
-        a_x[_] = icm20649.acceleration[0]
-        a_y[_] = icm20649.acceleration[1]
-        a_z[_] = icm20649.acceleration[2]
-        g_x[_] = icm20649.gyro[0]
-        g_y[_] = icm20649.gyro[1]
-        g_z[_] = icm20649.gyro[2]
+        a_x[_] = round(icm20649.acceleration[0], 4)
+        a_y[_] = round(icm20649.acceleration[1], 4)
+        a_z[_] = round(icm20649.acceleration[2], 4)
+        g_x[_] = round(icm20649.gyro[0], 4)
+        g_y[_] = round(icm20649.gyro[1], 4)
+        g_z[_] = round(icm20649.gyro[2], 4)
+        b[_] = round(bmp388.pressure, 4)
+        t[_] = round(bmp388.temperature, 4)
+        a[_] = round(bmp388.altitude, 4)
         _ += 1
-        time.sleep(0.1)
+        time.sleep(.005)
+        #print('Write...')
         if _ is 100: break
+
+    print('End test!')
+    end = time.time()
+    duration = end - start
+    print(f'test took {duration} seconds to complete.')
+
+    return b, t, a, a_x, a_y, a_z, g_x, g_y, g_z
+
     # end test
-    return a_x, a_y, a_z, g_x, g_y, g_z
 
 def mk_run_dir():
     """
@@ -157,8 +155,7 @@ def gather_store_data():
 
     run_path = mk_run_dir()
 
-    b, t, a = get_bta()
-    a_x, a_y, a_z, g_x, g_y, g_z = get_accel_gyro()
+    b, t, a, a_x, a_y, a_z, g_x, g_y, g_z = get_data()
 
     data_dict = {
             "Barometric Pressure":b,
