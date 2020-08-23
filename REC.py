1f0c5e4c3115f5210c2dacd71b547e3b58d82c3d
WildmanJames <dman13195@gmail.com>
2020-08-23 16:48:36 -0400

    Now takes less time!

--- REC.py
+++ REC.py
@@ -25,24 +25,17 @@ bmp388 = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 icm20649 = adafruit_icm20x.ICM20649(i2c)
 
 def main():
-
     print('Program start----------')
 
-    setup_boards()
+    g_max, g_min = setup()
 
-    determine_launch()
-
-    # while not determine_landed():
-    # figure out how to keep only data from 3 seconds before launch
-    # perhaps log data from when program starts and determine time of launch, then sort the big csv and remove data
-    # from before the launch
+    determine_launch(g_max, g_min)
 
     print('Program end----------')
 
     return None
 
-def setup_boards():
-
+def setup():
     global bmp388
     global icm20649
 
@@ -56,29 +49,46 @@ def setup_boards():
     print('Initializing, please wait...')
     time.sleep(5)
 
+    grav_vals = []
+    for i in range(100):
+        resultant = 0
+        for j in range(3):
+            resultant = resultant + (icm20649.acceleration[j] ** 2)
+        resultant = resultant ** 0.5
+        grav_vals.append(resultant)
+    grav_vals.sort(reverse = True)
+    avg_grav = (sum(grav_vals) / 100)
+    max_grav = grav_vals[0]
+    min_grav = grav_vals[len(grav_vals) - 1]
+
+    print(f'avg = {avg_grav}')
+    print(f'max = {max_grav}')
+    print(f'min = {min_grav}')
+
     print('Beginning test...')
 
-def determine_launch():
+    return max_grav, min_grav
+
+def determine_launch(g_max, g_min):
     """
     Will find the time of launch to be used in sorting data
     """
-    #print('Waiting for launch...')
-    #while True:
-        #if icm20649.acceleration[0] or icm20649.acceleration[1] or icm20649.acceleration[2] >= 10:
-            #print('Launch!!!')
-            #gather_store_data()
-    print('Launch!!!')
-    gather_store_data()
+
+    while True:
+        resultant = 0
+        for j in range(3):
+            resultant = resultant + (icm20649.acceleration[j] ** 2)
+        resultant = resultant ** 0.5
+
+        if (resultant > (g_max + .5)) or (resultant < (g_min - .5)):
+            print(f'Launch!!! --- {resultant}')
+            break
+
+    gather_store_data(g_max, g_min)
 
     return None
 
-def determine_landed():
-    """
-    Will find the time of landing to be used in ending recording
-    """
-    return None
-
-def get_data():
+def get_data(g_max, g_min):
     """
     This function will interact with bmp388 and return the barometric pressure and temperature detected by the
     sensor. It will also return the current altitude calculated by its built in altitude function
@@ -108,10 +118,10 @@ def get_data():
     g_z = []
 
     # test
-    start = time.time()
+    test_start = time.time()
+    landing_test = []
+    lt_index = 0
     while True:
-        if time.time() >= (start + 60):
-            break
         a_x.append(icm20649.acceleration[0])
         a_y.append(icm20649.acceleration[1])
         a_z.append(icm20649.acceleration[2])
@@ -122,9 +132,19 @@ def get_data():
         t.append(bmp388.temperature)
         a.append(bmp388.altitude)
 
+        # Determine if landed
+        resultant = ((a_x[lt_index] ** 2) + (a_y[lt_index] ** 2) + (a_z[lt_index] ** 2)) ** 0.5
+        landing_test.append(resultant)
+
+        if all((i < (g_max + .5)) and (i > (g_min - .5)) for i in landing_test[-10:]):
+            if lt_index is 10:
+                break
+
+        lt_index += 1
+
     print('End test!')
-    end = time.time()
-    duration = end - start
+    test_end = time.time()
+    duration = test_end - test_start
     print(f'Testing took {duration} seconds to complete.')
 
     return b, t, a, a_x, a_y, a_z, g_x, g_y, g_z
@@ -151,26 +171,27 @@ def mk_run_dir():
     # log print(f'Folder Created, {folder_name} @ {run_path}')
     return run_path
 
-def gather_store_data():
+def gather_store_data(g_max, g_min):
     """
-    This function will gather the data and create a .csv file to save all the data into, each data type will have its own
+    This function will gather the data and create a .csv file to save all the data into, each data type will have its
+    own
     and column. Each consecutive data point will be stored in rows under their respective columns
     """
 
     run_path = mk_run_dir()
 
-    b, t, a, a_x, a_y, a_z, g_x, g_y, g_z = get_data()
+    b, t, a, a_x, a_y, a_z, g_x, g_y, g_z = get_data(g_max,g_min)
 
     data_dict = {
-            "Barometric Pressure":b,
-            "Temperature":t,
-            "Altitude_B":a,
-            "Accelerometer_x":a_x,
-            "Accelerometer_y":a_y,
-            "Accelerometer_z":a_z,
-            "Gyroscope_x":g_x,
-            "Gyroscope_y":g_y,
-            "Gyroscope_z":g_z,
+        "Barometric Pressure":b,
+        "Temperature"        :t,
+        "Altitude_B"         :a,
+        "Accelerometer_x"    :a_x,
+        "Accelerometer_y"    :a_y,
+        "Accelerometer_z"    :a_z,
+        "Gyroscope_x"        :g_x,
+        "Gyroscope_y"        :g_y,
+        "Gyroscope_z"        :g_z,
         }
     data = pd.DataFrame.from_dict(data_dict)
 
